934978bae21a9519d7165f56d37b3ab4ba4361b1
Nathan Tolbert <ntolbertu85@gmail.com>
2021-07-24 16:26:24 -0400

    07242021 -- project-scope -- INITIAL-build project & create 2 .c files + docker-compose.yml

---
+++ serialize.c
@@ -0,0 +1,16 @@
+// serialize.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct person{
+    char name[20];
+    int age;
+    char gender;
+} Person;
+
+
+int main(int argc, char* argv[]) {
+
+}
